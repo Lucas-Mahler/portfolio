9973e5bdd5821aa500c910a41f5c05717e8718bb
lucas-mahler <lucas.mahler.dev@gmail.com>
2024-08-31 14:24:41 +0200

    main.ts push

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { appConfig } from "./app/app.config";
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes, withHashLocation())
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
